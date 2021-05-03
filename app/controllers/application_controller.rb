2bb1b496f1f9239f68a63c98a42dda5fe575dc14
Cassandra <cparisi1290@gmail.com>
2021-05-03 16:45:57 -0500

    add mock current user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API 
+
+    def current_user
+        # hardcode user
+        User.first
+    end
+
+    def logged_in?
+        # mock current user
+        !!current_user
+    end
 end

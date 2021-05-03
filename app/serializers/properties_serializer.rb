528aaa612a7637eb1aeac237c82eb3a98cb865f2
Cassandra <cparisi1290@gmail.com>
2021-05-03 16:06:27 -0500

    create property serializer

---
+++ app/serializers/properties_serializer.rb
@@ -0,0 +1,4 @@
+class PropertiesSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :address, :city, :state, :zip, :bedrooms, :bathrooms, :image, :occupied?, :pets_allowed?, :rent_amount, :user_id, :user
+end

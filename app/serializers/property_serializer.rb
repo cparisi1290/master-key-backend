b0e584693e58b84952364463f7ebc1d125422e5e
Cassandra <cparisi1290@gmail.com>
2021-05-03 16:43:44 -0500

    add association to repairs

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -1,4 +1,5 @@
 class PropertySerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :address, :city, :state, :zip, :bedrooms, :bathrooms, :image, :occupied?, :pets_allowed?, :rent_amount, :user_id
+  has_many :repairs
 end

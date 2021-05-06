3d3c9d384ff1d493a8c980ef875387507341ee28
Cassandra <cparisi1290@gmail.com>
2021-05-06 14:51:14 -0500

    add relationship to repairs as attributes

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -1,5 +1,18 @@
 class PropertySerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :address, :city, :state, :zip, :bedrooms, :bathrooms, :image, :occupied?, :pets_allowed?, :rent_amount, :user_id
-  has_many :repairs
+  # has_many :repairs, serializer: RepairSerializer
+
+  attribute :repairs do |property|
+    property.repairs.map do |repair| 
+      {
+        created_at: repair.created_at,
+        room: repair.room,
+        broken_item: repair.broken_item,
+        cost: repair.cost,
+        date_completed: repair.date_completed,
+        closed?: repair.closed?
+      }
+    end
+  end
 end

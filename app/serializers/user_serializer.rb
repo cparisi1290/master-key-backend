6084bf7c2701507bef52cb399f55aee6211d238a
Cassandra <cparisi1290@gmail.com>
2021-05-06 14:51:34 -0500

    add relationship to properties as attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,23 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :email
-  has_many :properties
+  # has_many :properties, serializer: PropertySerializer
+
+  attribute :properties do |user|
+    user.properties.map do |property| 
+      {
+        name: property.name, 
+        address: property.address,
+        city: property.city,
+        state: property.state,
+        zip: property.zip,
+        bedrooms: property.bedrooms,
+        bathrooms: property.bathrooms,
+        image: property.image,
+        occupied?: property.occupied?,
+        pets_allowed?: property.pets_allowed?,
+        rent_amount: property.rent_amount,
+      }
+    end
+  end
 end
